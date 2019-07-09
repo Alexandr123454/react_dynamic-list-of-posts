4da12f88ae12aec47e57e1e8798e83791a175674
alexandr <alexandrov6927@gmail.com>
2019-07-09 21:53:16 +0300

    Updates

---
+++ precache-manifest.aeae5c90ba624463d3ed571e79eb780f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c7f9bb65782d8c83cf54d094425620",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "a00b785fd329b5ee9384",
+    "url": "/react_dynamic-list-of-posts/static/css/main.5a7b8119.chunk.css"
+  },
+  {
+    "revision": "5f5a53db3ca51547e5fd",
+    "url": "/react_dynamic-list-of-posts/static/js/2.0122b471.chunk.js"
+  },
+  {
+    "revision": "a00b785fd329b5ee9384",
+    "url": "/react_dynamic-list-of-posts/static/js/main.56ca24d1.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
